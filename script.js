f70222c6be5275c764942229593da0b9197cbfa4
philip0980 <127869236+philip0980@users.noreply.github.com>
2023-12-05 01:03:05 +0530

    typingGame

---
+++ script.js
@@ -0,0 +1,107 @@
+const hello = document.getElementById("hello");
+const input = document.querySelector(".inputField");
+
+let typedWord = "";
+let score = 1;
+let level = 61;
+let level1 = 1200;
+let intervalId;
+
+const names = [
+  "alochan",
+  "philip",
+  "pooja",
+  "manish",
+  "nikesh",
+  "parnav",
+  "amash",
+  "niraj",
+  "bipin",
+  "amit",
+  "amandeep",
+  "dosangla",
+  "sweety",
+  "akansha",
+  "palak",
+  "prachi",
+  "anshika",
+  "priyanshu",
+  "sameer",
+  "lisa",
+  "arbin",
+  "biraj",
+];
+
+function setNewInterval() {
+  clearInterval(intervalId); // Clear the existing interval
+  intervalId = setInterval(() => {
+    random();
+  }, level1);
+}
+
+setNewInterval(); // Initial setup
+
+function random() {
+  const index = Math.floor(Math.random() * names.length);
+  const span = Span(names[index]);
+  document.body.appendChild(span);
+}
+
+function Span(word) {
+  const span = document.createElement("span");
+  span.innerHTML = word;
+  span.id = word;
+  let pos = Math.floor(Math.random() * (window.innerWidth - 100));
+  span.style.position = "absolute";
+  span.style.left = pos + "px";
+  span.style.top = "60px";
+  document.body.appendChild(span);
+
+  const dropIntervalId = setInterval(() => {
+    drop(span, dropIntervalId);
+  }, level);
+
+  span.dropIntervalId = dropIntervalId;
+
+  function drop(span) {
+    let top = parseFloat(span.style.top);
+    top++;
+    span.style.top = top + "px";
+
+    if (top > window.innerHeight) {
+      console.log("Game over ");
+      alert(`Game over 😭\nYour score is ${hello.innerHTML}`);
+      alert("Press OK to play again");
+      clearInterval(dropIntervalId);
+      span.remove();
+      location.reload();
+    }
+  }
+
+  return span;
+}
+
+input.addEventListener("input", (event) => {
+  typedWord = input.value.trim();
+
+  if (typedWord !== "") {
+    const spans = document.querySelectorAll("span");
+    spans.forEach((span) => {
+      if (typedWord === span.id) {
+        clearInterval(span.dropIntervalId);
+        span.remove();
+        hello.innerHTML = score++;
+
+        if (score % 5 === 0) {
+          level -= 10;
+          if (level1 >= 600) {
+            level1 -= 50;
+          }
+          setNewInterval();
+        }
+
+        input.value = "";
+      }
+    });
+  }
+});
