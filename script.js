bbd6bbebe581bc2efb171d894517c1e54e66e4ad
philip0980 <127869236+philip0980@users.noreply.github.com>
2023-12-12 18:39:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ if (!localStorage.getItem("userName")) {
 function sendScoreToBackend(username, score) {
   const data = { name: username, score };
 
- fetch("http://localhost:8000", {
+ fetch("https://bunkerstypingbackend.onrender.com/", {
     method: "POST",
     headers: {
       'Accept': 'application/json',
